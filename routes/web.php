336c9f77975c7450fe5d97ced5acaaf7cadab637
HossSalama <hossamsalama706@gmail.com  git config --global user.name Hossam>
2025-10-02 03:31:19 +0300

    Commit

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
-
-Route::get('/fix-storage-link', function () {
-    Artisan::call('storage:link');
-    return '✅ storage link created successfully.';
-});
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
