98a04943d491f3f132397a4762b71b9147435cd9
HossSalama <hossamsalama706@gmail.com  git config --global user.name Hossam>
2025-10-02 09:30:04 +0300

    Commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            'throttle:120,1',
             \App\Http\Middleware\SetApiLocale::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Illuminate\Http\Middleware\HandleCors::class,
