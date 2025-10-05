fcbef10bb03c13874bff158185db05884d8f0140
HossSalama <hossamsalama706@gmail.com  git config --global user.name Hossam>
2025-10-06 02:55:19 +0300

    Commit

--- app/Http/Requests/StoreMainCategoriesRequest.php
+++ app/Http/Requests/StoreMainCategoriesRequest.php
@@ -14,10 +14,10 @@ class StoreMainCategoriesRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'categories' => 'required|array|min:1',
+            'categories' => 'nullable|array|min:1',
             'categories.*.brand_id' => 'required|exists:brands,id',
             'categories.*.name_en' => 'required|string|max:255',
-            'categories.*.name_ar' => 'required|string|max:255',
+            'categories.*.name_ar' => 'nullable|string|max:255',
             'categories.*.image_url' => 'nullable|image|mimes:jpeg,png,jpg,webp|max:2048',
             'categories.*.color_code' => 'nullable|string|max:7',
         ];
